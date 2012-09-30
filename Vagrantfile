41bc7724a44855e86550defc8125dee49ee8991f
Henry Garner <henry@henrygarner.com>
2012-09-30 20:17:05 +0100

    Update for precise and cdh4

--- Vagrantfile
+++ Vagrantfile
@@ -2,8 +2,8 @@
 # vi: set ft=ruby :
 
 Vagrant::Config.run do |config|
-  config.vm.box = "lucid64"
-  config.vm.box_url = "http://files.vagrantup.com/lucid64.box"
+  config.vm.box = "precise64"
+  config.vm.box_url = "http://dl.dropbox.com/u/1537815/precise64.box"
 
   config.vm.provision :shell, :inline => "apt-get update"
 
--- chef/okapi/recipes/default.rb
+++ chef/okapi/recipes/default.rb
@@ -1,11 +1,13 @@
 node[:java][:java_home] = "/usr/lib/jvm/java-6-openjdk"
 
+node[:hadoop][:distribution] = "cdh4"
+
 node[:maven][:version] = '3'
-node[:maven]['3'][:url] = "http://mirrors.comunicare-europe.co.uk/apache/maven/maven-3/3.0.4/binaries/apache-maven-3.0.4-bin.tar.gz"
+node[:maven]['3'][:url] = "http://apache.mirrors.timporter.net/maven/maven-3/3.0.4/binaries/apache-maven-3.0.4-bin.tar.gz"
 node[:maven]['3'][:checksum] = nil # "e513740978238cb9e4d482103751f6b7"
 
 include_recipe "git"
-include_recipe "hadoop"
+include_recipe "hadoop::conf_pseudo"
 include_recipe "maven"
 
 git "/home/vagrant/giraph" do
