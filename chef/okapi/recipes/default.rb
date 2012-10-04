f3d0929d2ad79a17e939b06b06709ca6370930c6
Henry Garner <henry@henrygarner.com>
2012-10-04 13:07:25 +0100

    Install maven, but stick with Oracle java

--- chef/okapi/recipes/default.rb
+++ chef/okapi/recipes/default.rb
@@ -1,10 +1,8 @@
-node[:java][:java_home] = "/usr/lib/jvm/java-6-openjdk"
+node[:hadoop][:distribution] = "cdh3"
 
-node[:hadoop][:distribution] = "cdh4"
-
-node[:maven][:version] = '3'
-node[:maven]['3'][:url] = "http://apache.mirrors.timporter.net/maven/maven-3/3.0.4/binaries/apache-maven-3.0.4-bin.tar.gz"
-node[:maven]['3'][:checksum] = nil # "e513740978238cb9e4d482103751f6b7"
+node[:maven][:version] = "3"
+node[:maven]['3'][:url] = "http://mirror.catn.com/pub/apache/maven/maven-3/3.0.4/binaries/apache-maven-3.0.4-bin.tar.gz"
+node[:maven]['3'][:checksum] = nil
 
 include_recipe "git"
 include_recipe "hadoop::conf_pseudo"
@@ -14,12 +12,6 @@ git "/home/vagrant/giraph" do
   repository "git://git.apache.org/giraph.git"
   revision "HEAD"
   action :sync
-  user 'vagrant'
-  group 'vagrant'
-end
-
-execute "mvn install -DskipTests" do
-  cwd "/home/vagrant/giraph"
   user 'vagrant'
   group 'vagrant'
 end
